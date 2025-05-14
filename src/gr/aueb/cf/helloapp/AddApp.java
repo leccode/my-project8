c61e3b628fbb87844febd3f9e1a9c8f7195c6bed
leccode <costas.lekkos@gmail.com>
2025-05-14 19:40:34 +0300

    feat: add addition class

---
+++ src/gr/aueb/cf/helloapp/AddApp.java
@@ -0,0 +1,24 @@
+package gr.aueb.cf.helloapp;
+
+/**
+ * New feature to add 2 integers.
+ * Add method is called in main.
+ *
+ * @since 14/05/25
+ * @author Constantine Lekkos
+ */
+public class AddApp {
+    public static void main(String[] args) {
+        System.out.println(add(3, 5));
+    }
+
+    /**
+     * Add method of 2 integers.
+     * @param a First integer.
+     * @param b Second integer.
+     * @return An integer is returned as a result.
+     */
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
